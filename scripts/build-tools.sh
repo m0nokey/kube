0f403b16b6157fed4e4b9374425d31f591639c7f
m0nokey <218000218+m0nokey@users.noreply.github.com>
2026-09-09 17:56:44 +0000

    Use verified releases for auxiliary tools

--- scripts/build-tools.sh
+++ scripts/build-tools.sh
@@ -55,31 +55,42 @@ fetch_release kubernetes/kubernetes /src/kubernetes "v${KUBECTL_VERSION}" "${KUB
     go build -trimpath -ldflags "${version_ldflags}" -o /out/kubeadm ./cmd/kubeadm
 )
 
-fetch_release kubernetes-sigs/cri-tools /src/cri-tools "v${CRICTL_VERSION}" "${CRICTL_COMMIT}"
-(
-    cd /src/cri-tools
-    GOFLAGS= go get golang.org/x/net@v0.58.0 golang.org/x/sys@v0.48.0 golang.org/x/text@v0.42.0
-    export GOFLAGS=-mod=mod
-    go build -trimpath -ldflags "-s -w" -o /out/crictl ./cmd/crictl
-)
+case "${TARGETARCH}" in
+    amd64)
+        CRICTL_SHA256=83855e114566a8a8c44c548d515670f51de3a5e1da8b2effb59870e2f10c25a3
+        ETCD_SHA256=e8cd3fa8064c98137c5dbd78b76f969417ace84efb83c481041d7a52ffdd8fb9
+        CLUSTERCTL_SHA256=01122674fd3c47a33206ab1b8b81d437afbcf5dd25d126535564f24a2cdf676e
+        ;;
+    arm64)
+        CRICTL_SHA256=68328594ccf780a80ae2b092d9f6ce484eec7cf540c275242e8fd954bfd95332
+        ETCD_SHA256=d7e25e08f694b6ed7792fc7b7a891fe2c3f3d3dccfe2f3bfdb1547b0eb75b6da
+        CLUSTERCTL_SHA256=83976008aa9ddb81dab01443c646aaa125e4993e17bf24e790e29779f712d79d
+        ;;
+esac
 
-fetch_release etcd-io/etcd /src/etcd "v${ETCD_VERSION}" "${ETCD_COMMIT}"
-(
-    cd /src/etcd
-    export GOWORK=off
-    GOFLAGS= go get golang.org/x/crypto@v0.57.0 golang.org/x/net@v0.58.0 golang.org/x/sys@v0.48.0 golang.org/x/text@v0.42.0
-    export GOFLAGS=-mod=mod
-    go build -trimpath -ldflags "-s -w" -o /out/etcdctl ./etcdctl
-    go build -trimpath -ldflags "-s -w" -o /out/etcdutl ./etcdutl
-)
+download_checked() {
+    url="$1"
+    expected="$2"
+    output="$3"
+    curl -fsSL --retry 5 --retry-delay 2 "${url}" -o "${output}"
+    printf '%s  %s\n' "${expected}" "${output}" | sha256sum -c -
+}
 
-fetch_release kubernetes-sigs/cluster-api /src/cluster-api "v${CLUSTERCTL_VERSION}" "${CLUSTERCTL_COMMIT}"
-(
-    cd /src/cluster-api
-    GOFLAGS= go get golang.org/x/crypto@v0.57.0 golang.org/x/net@v0.58.0 golang.org/x/sys@v0.48.0 golang.org/x/text@v0.42.0
-    export GOFLAGS=-mod=mod
-    go build -trimpath -ldflags "-s -w" -o /out/clusterctl ./cmd/clusterctl
-)
+crictl_archive="/tmp/crictl-v${CRICTL_VERSION}-linux-${TARGETARCH}.tar.gz"
+download_checked "https://github.com/kubernetes-sigs/cri-tools/releases/download/v${CRICTL_VERSION}/crictl-v${CRICTL_VERSION}-linux-${TARGETARCH}.tar.gz" "${CRICTL_SHA256}" "${crictl_archive}"
+tar -xzf "${crictl_archive}" -C /out
+
+etcd_archive="/tmp/etcd-v${ETCD_VERSION}-linux-${TARGETARCH}.tar.gz"
+download_checked "https://github.com/etcd-io/etcd/releases/download/v${ETCD_VERSION}/etcd-v${ETCD_VERSION}-linux-${TARGETARCH}.tar.gz" "${ETCD_SHA256}" "${etcd_archive}"
+tar -xzf "${etcd_archive}" -C /tmp
+cp "/tmp/etcd-v${ETCD_VERSION}-linux-${TARGETARCH}/etcdctl" /out/etcdctl
+cp "/tmp/etcd-v${ETCD_VERSION}-linux-${TARGETARCH}/etcdutl" /out/etcdutl
+rm -rf "/tmp/etcd-v${ETCD_VERSION}-linux-${TARGETARCH}" "${crictl_archive}" "${etcd_archive}"
+
+clusterctl_binary="/tmp/clusterctl-linux-${TARGETARCH}"
+download_checked "https://github.com/kubernetes-sigs/cluster-api/releases/download/v${CLUSTERCTL_VERSION}/clusterctl-linux-${TARGETARCH}" "${CLUSTERCTL_SHA256}" "${clusterctl_binary}"
+cp "${clusterctl_binary}" /out/clusterctl
+rm -f "${clusterctl_binary}"
 
 fetch_release kubernetes-sigs/kustomize /src/kustomize "kustomize/v${KUSTOMIZE_VERSION}" "${KUSTOMIZE_COMMIT}"
 (

cb3b11b9024ab88e45218251a5f00318e4688266
m0nokey <218000218+m0nokey@users.noreply.github.com>
2026-09-09 17:41:52 +0000

    Avoid tidy in multi-module tool builds

--- scripts/build-tools.sh
+++ scripts/build-tools.sh
@@ -59,7 +59,6 @@ fetch_release kubernetes-sigs/cri-tools /src/cri-tools "v${CRICTL_VERSION}" "${C
 (
     cd /src/cri-tools
     GOFLAGS= go get golang.org/x/net@v0.58.0 golang.org/x/sys@v0.48.0 golang.org/x/text@v0.42.0
-    GOFLAGS= go mod tidy
     export GOFLAGS=-mod=readonly
     go build -trimpath -ldflags "-s -w" -o /out/crictl ./cmd/crictl
 )
@@ -68,7 +67,6 @@ fetch_release etcd-io/etcd /src/etcd "v${ETCD_VERSION}" "${ETCD_COMMIT}"
 (
     cd /src/etcd
     GOFLAGS= go get golang.org/x/crypto@v0.57.0 golang.org/x/net@v0.58.0 golang.org/x/sys@v0.48.0 golang.org/x/text@v0.42.0
-    GOFLAGS= go mod tidy
     export GOFLAGS=-mod=readonly
     go build -trimpath -ldflags "-s -w" -o /out/etcdctl ./etcdctl
     go build -trimpath -ldflags "-s -w" -o /out/etcdutl ./etcdutl
@@ -78,7 +76,6 @@ fetch_release kubernetes-sigs/cluster-api /src/cluster-api "v${CLUSTERCTL_VERSIO
 (
     cd /src/cluster-api
     GOFLAGS= go get golang.org/x/crypto@v0.57.0 golang.org/x/net@v0.58.0 golang.org/x/sys@v0.48.0 golang.org/x/text@v0.42.0
-    GOFLAGS= go mod tidy
     export GOFLAGS=-mod=readonly
     go build -trimpath -ldflags "-s -w" -o /out/clusterctl ./cmd/clusterctl
 )
